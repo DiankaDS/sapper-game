5db61b584a2b2eeea2ae011ecab135db409e44aa
Diana <dianka.agafonova@gmail.com>
2020-10-08 21:11:05 +0300

    Update menu, create game board

--- sapper.js
+++ sapper.js
@@ -15,13 +15,18 @@ class Sapper {
         white: '#ffffff',
     };
 
-    constructor() {
+    constructor(rows, bombs) {
+        this.rows = rows;
+        this.bombs = bombs;
+        this.sizeElem = 30;
+        this.size = this.sizeElem * this.rows;
         this.canvas = document.getElementById('draw');
         this.context = this.canvas.getContext('2d');
         this.isGame = true;
         this.newGameButton = this.setButton(this.newGameButton, 66);
         this.menuButton = this.setButton(this.menuButton, 20);
         this.setListeners();
+        this.startGame(this.context);
     }
 
     setListeners() {
@@ -32,10 +37,6 @@ class Sapper {
         button = new Path2D();
         button.rect(210, 30*16 + i, 90, 40);
         button.closePath();
-        
-        this.context.fill(button);
-        this.context.stroke(button);
-
         return button;
     }
 
@@ -44,7 +45,7 @@ class Sapper {
         if (this.context.isPointInPath(this.menuButton, pointer.x, pointer.y) && this.isGame) {
             this.showHowToPlay(this.context);
         } else if (this.context.isPointInPath(this.newGameButton, pointer.x, pointer.y)) {
-            console.log('game');
+            this.startGame(this.context);
         }
     }
 
@@ -55,12 +56,31 @@ class Sapper {
         return {x:x, y:y};
     }
 
-    showHowToPlay(x) {
+    clearRect(x) {
         x.clearRect(0, 0, this.canvas.width, this.canvas.height);
+    }
+
+    showHowToPlay(x) {
+        this.clearRect(x);
         this.isGame = false;
         let size = 450;
         let width = this.canvas.width;
         let height = this.canvas.height;
+
+        let mainText = [
+            'To be successful in this game you need to try and',
+            'create an opening in the map to which you will see',
+            'numbers appear indicating to you if any bombs are',
+            'around that square. Segregate suspicious square',
+            'before placing your flags and finding all of the bombs.',
+        ];
+
+        let controlText = [
+            'Press left mouse button to open a tile',
+            'Press right mouse button to place a flag',
+            'Press left mouse button twice to open a tiles',
+            'around the number',
+        ];
     
         x.save();
 
@@ -74,18 +94,9 @@ class Sapper {
     
         x.textAlign = 'left';
         x.font = this.font.header;
-    
-        x.fillStyle = this.color.green;
-        x.fillText('To be successful in this game you need to try and', 0, height * 1.5 / 10);
-        x.fillText('create an opening in the map to which you will see', 0, height * 2 / 10);
-        x.fillText('numbers appear indicating to you if any bombs are', 0, height * 2.5 / 10);
-        x.fillText('around that square. Segregate suspicious square', 0, height * 3 / 10);
-        x.fillText('before placing your flags and finding all of the bombs.', 0, height * 3.5 / 10);
-        x.fillStyle = this.color.orange;
-        x.fillText('Press left mouse button to open a tile', 0, height * 5.5 / 10);
-        x.fillText('Press right mouse button to place a flag', 0, height * 6 / 10);
-        x.fillText('Press left mouse button twice to open a tiles around', 0, height * 6.5 / 10);
-        x.fillText('the number', 0, height * 7 / 10);
+
+        this.fillHowToText(x, mainText, height, 1, this.color.green);
+        this.fillHowToText(x, controlText, height, 5, this.color.orange);
     
         x.strokeStyle = 'lightgrey';
         x.fillStyle = this.color.blue;
@@ -100,11 +111,56 @@ class Sapper {
         x.restore();
     }
 
-    game() {
-        console.log(this.isGame);
+    fillHowToText(x, text, height, heightIndex, color) {
+        x.fillStyle = color;
+
+        for (let i in text) {
+            x.fillText(text[i], 0, height * (heightIndex += 0.5) / 10);
+        }
+    }
+
+    fillGameFooter(x) {
+        x.save();
+
+        x.font = this.font.medium;
+        x.fillStyle = this.color.blue;
+        x.textAlign = 'left';
+
+        x.strokeRect(10, this.size + 20, 95, 40);
+        x.strokeText('Bombs', 15, this.size + 50);
+        x.strokeRect(10, this.size + 62, 95, 45);
+        x.strokeText('x' + this.bombs, 30, this.size + 90);
+
+        x.strokeRect(110, this.size + 20, 95, 40);
+        x.strokeText('Time', 125, this.size + 50);
+        x.strokeRect(110, this.size + 62, 95, 45);
+        x.strokeText('000', 130, this.size + 90);
+
+        x.font = this.font.small;
+        x.strokeStyle = 'lightgrey';
+
+        x.fill(this.menuButton);
+        x.stroke(this.menuButton);
+        x.fill(this.newGameButton);
+        x.stroke(this.newGameButton);
+
+        x.fillStyle = this.color.white;
+        x.fillText('New game', 220, this.size + 90);
+        x.fillText('How to play', 218, this.size + 45);
+
+        x.restore();
+    }
+
+    startGame(x) {
+        this.clearRect(x);
+        this.isGame = true;
+
+        x.strokeRect(0, 0, this.size, this.size);
+
+        this.fillGameFooter(x);
+
     }
 
 }
 
-const sapper = new Sapper();
-sapper.game();
+const sapper = new Sapper(16, 40);
