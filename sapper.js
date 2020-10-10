4be49da5e91c7a94b675874fc28e4dd8dd4ee1bb
Diana <dianka.agafonova@gmail.com>
2020-10-10 22:39:20 +0300

    Add images, add mouse events

--- sapper.js
+++ sapper.js
@@ -13,6 +13,8 @@ class Sapper {
         orange: '#ee8149',
         blue: '#3c6b6d',
         white: '#ffffff',
+        winShadow: '#0ae80d',
+        loseShadow: '#F00',
     };
 
     constructor(rows, bombs) {
@@ -23,11 +25,27 @@ class Sapper {
         this.canvas = document.getElementById('draw');
         this.context = this.canvas.getContext('2d');
         this.isGame = true;
+        this.field = [];
+        this.flags = [];
+        this.openElems = [];
+
         this.newGameButton = this.setButton(this.newGameButton, 66);
         this.menuButton = this.setButton(this.menuButton, 20);
 
         this.image = new Image();
         this.image.src = './assets/s-point.png';
+
+        this.bomb1 = new Image();
+        this.bomb1.src = './assets/bomb1.png';
+
+        this.bomb0 = new Image();
+        this.bomb0.src = './assets/bomb0.png';
+
+        this.bomb2 = new Image();
+        this.bomb2.src = './assets/bomb2.png';
+
+        this.flag = new Image();
+        this.flag.src = './assets/flag.png';
 
         this.setListeners();
         this.image.onload = () => {
@@ -37,6 +55,8 @@ class Sapper {
 
     setListeners() {
         document.addEventListener('click', e => this.getButtonAction(e), false);
+        this.canvas.addEventListener('click', e => this.openCell(e.layerX, e.layerY));
+        this.canvas.oncontextmenu = e => this.putOrRemoveFlag(e.layerX, e.layerY);
     }
 
     setButton(button, i) {
@@ -48,7 +68,7 @@ class Sapper {
 
     getButtonAction(e) {
         let pointer = this.getPointer(this.canvas, e);
-        if (this.context.isPointInPath(this.menuButton, pointer.x, pointer.y) && this.isGame) {
+        if (this.context.isPointInPath(this.menuButton, pointer.x, pointer.y)) {
             this.showHowToPlay(this.context);
         } else if (this.context.isPointInPath(this.newGameButton, pointer.x, pointer.y)) {
             this.startGame(this.context);
@@ -126,22 +146,25 @@ class Sapper {
     }
 
     startGame(x) {
-        this.clearRect(x);
-        this.isGame = true;
+        this.flags = [];
+        this.openElems = [];
 
+        this.clearRect(x);
         x.strokeRect(0, 0, this.size, this.size);
 
-        let field = this.putRandomBombs(this.createMatrix());
-        field = this.putNumbers(field);
+        let field = this.putNumbers(this.putRandomBombs(this.createMatrix()));
 
         this.drawGameBoard(x);
         this.fillGameFooter(x);
+
+        this.field = field;
+        this.isGame = true;
 
         // === for testing
         // x.fillStyle = 'red';
         // for (let t1 = 0; t1 < this.rows; t1 ++) {
         //     for (let t2 = 0; t2 < this.rows; t2 ++) {
-        //         x.fillText(field[t2][t1], t2 * 30 + 10, t1 * 30 + 20);
+        //         x.fillText(this.field[t2][t1], t2 * 30 + 10, t1 * 30 + 20);
         //     }
         // }
         // === for testing
@@ -232,6 +255,148 @@ class Sapper {
 
         x.restore();
     }
+
+    putOrRemoveFlag(layerX, layerY) {
+        let xElem = Math.floor(layerX / this.sizeElem);
+        let yElem = Math.floor(layerY / this.sizeElem);
+
+        if (
+            this.isGame &&
+            xElem < this.rows &&
+            yElem < this.rows 
+            && this.openElems.indexOf(xElem + ' ' + yElem) == -1
+        ) {
+            let elemId = this.flags.indexOf(xElem + ' ' + yElem);
+            if (elemId == -1) {
+                this.context.drawImage(this.flag, xElem * this.sizeElem, yElem * this.sizeElem, this.sizeElem, this.sizeElem);
+                this.flags.push(xElem + ' ' + yElem);
+            } else {
+                this.context.drawImage(this.image, xElem * this.sizeElem, yElem * this.sizeElem, this.sizeElem, this.sizeElem);
+                this.flags.splice(elemId, 1);
+            }
+
+            this.context.fillStyle = this.color.white;
+            this.context.fillRect(12, this.size + 64, 92, 42);
+            this.context.font = this.font.medium;
+            this.context.fillStyle = this.color.blue;
+            this.context.textAlign = 'left';
+            this.context.strokeText('x' + (this.bombs - this.flags.length), 30, this.size + 90);
+        }
+        return false;
+    }
+
+    openCell(layerX, layerY) {
+        let xElem = Math.floor(layerX / this.sizeElem);
+        let yElem = Math.floor(layerY / this.sizeElem);
+
+        if (
+            this.isGame &&
+            xElem < this.rows &&
+            yElem < this.rows &&
+            this.flags.indexOf(xElem + ' ' + yElem) == -1 &&
+            this.openElems.indexOf(xElem + ' ' + yElem) == -1
+        ) {
+            let point = this.field[xElem][yElem];
+
+            if (point == 'x') {
+                this.isGame = false;
+                this.putAllBombs();
+                this.context.drawImage(this.bomb1, xElem * this.sizeElem, yElem * this.sizeElem, this.sizeElem, this.sizeElem);
+                this.drawLose();
+            } 
+            else if (point == '0') {
+                this.openEmpty(xElem, yElem);
+            } 
+            else {
+                this.context.fillStyle = 'lightgrey';
+                this.context.fillRect(xElem * this.sizeElem, yElem * this.sizeElem, this.sizeElem, this.sizeElem);
+                this.context.fillStyle = '#3c6b6d';
+                this.context.font = '15px Verdana';
+                this.context.fillText(point, xElem * this.sizeElem + 10, yElem * this.sizeElem + 20);
+
+                this.openElems.push(xElem + ' ' + yElem);
+            }
+
+            if (
+                this.isGame &&
+                this.openElems.length >= this.rows * this.rows - this.bombs
+            ) {
+                this.isGame = false;
+                this.putLastFlags();
+                this.drawWin();
+            }
+        }
+
+    }
+
+    putAllBombs() {
+        for (let i = 0; i < this.rows; i++) {
+            for (let j = 0; j < this.rows; j++) {
+                if (this.field[i][j] == 'x') {
+                    let elemId = this.flags.indexOf(i + ' ' + j);
+                    if (elemId == -1) {
+                        this.context.drawImage(this.bomb0, i * this.sizeElem, j * this.sizeElem, this.sizeElem, this.sizeElem);
+                    } else {
+                        this.flags.splice(elemId, 1);
+                    }
+                }
+            }
+        }
+
+        for (let i = 0; i < this.flags.length; i++) {
+            let a = this.flags[i].split(' ');
+            this.context.drawImage(this.bomb2, a[0] * this.sizeElem, a[1] * this.sizeElem, this.sizeElem, this.sizeElem);
+        }
+    }
+
+    putLastFlags() {
+        for (let i = 0; i < this.rows; i++) {
+            for (let j = 0; j < this.rows; j++) {
+                if (this.field[i][j] == 'x') {
+                    this.context.drawImage(this.flag, i * this.sizeElem, j * this.sizeElem, this.sizeElem, this.sizeElem);
+                }
+            }
+        }
+    }
+
+    openEmpty(xElem, yElem) {
+        console.log('empty');
+    }
+
+    drawWin() {
+        this.context.save();
+        this.context.textAlign = 'center';
+        this.context.font = this.font.large;
+        this.context.shadowColor = this.color.winShadow;
+        this.context.shadowOffsetX = 5;
+        this.context.shadowOffsetY = 5;
+        this.context.shadowBlur = 5;
+        this.context.strokeStyle = this.color.lightgreen;
+        this.context.lineWidth = 2;
+        this.context.strokeText('You win!', this.canvas.width / 2, this.canvas.height / 4);
+        this.context.restore();
+
+        this.context.fillStyle = 'white';
+        this.context.fillRect(12, this.size + 64, 92, 42);
+        this.context.font = this.font.medium;
+        this.context.fillStyle = this.color.blue;
+        this.context.textAlign = 'left';
+        this.context.strokeText('x0', 30, this.size + 90);
+    }
+
+    drawLose() {
+        this.context.save();
+        this.context.textAlign = 'center';
+        this.context.font = this.font.large;
+        this.context.shadowColor = this.color.loseShadow;
+        this.context.shadowOffsetX = 5;
+        this.context.shadowOffsetY = 5;
+        this.context.shadowBlur = 5;
+        this.context.strokeStyle = this.color.loseShadow;
+        this.context.lineWidth = 2;
+        this.context.strokeText('You lose!', this.canvas.width / 2, this.canvas.height / 4);
+        this.context.restore();
+    }
 }
 
-const sapper = new Sapper(16, 40);
+const sapper = new Sapper(16, 255);
